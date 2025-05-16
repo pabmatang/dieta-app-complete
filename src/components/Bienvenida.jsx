a03ccff5a8dfd78ffbdae9eb65516b51651f29b3
Pablo Mateos <73099537+pabmatang@users.noreply.github.com>
2025-05-16 12:24:04 +0200

    Update Bienvenida.jsx

--- src/components/Bienvenida.jsx
+++ src/components/Bienvenida.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Bienvenida.css"
+import logo from '../imagenes/logo.jpeg';
 
 function Bienvenida({ onStart }) {
   return (
     <div className="bienvenida-container">
       <h2>Bienvenido a la App Menú Saludable</h2>
-      <img src="../logo.jpeg" alt="Imagen bienvenida" className="bienvenida-imagen" />
+     <img src={logo} alt="Imagen bienvenida" className="bienvenida-imagen" />
       <p>Tu asistente para crear menús semanales personalizados y saludables.</p>
       <button onClick={onStart}>Empezar</button>
     </div>
